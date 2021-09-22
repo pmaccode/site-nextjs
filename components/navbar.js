d69df3e281ce5c7597a9d50c0755ec88ffd3d021
Diogo Franco <diogofrancoweb@gmail.com>
2021-09-22 09:46:27 -0300

    navar atualizada

--- components/navbar.js
+++ components/navbar.js
@@ -8,7 +8,7 @@ export function NavTop(){
     return(
       <Navbar className={StyleNav.navTop}>
         <Container>
-            <Col md={{ span: 4, offset: 8 }} className="text-end">
+            <Col xs md={{ span: 4, offset: 8 }} className="text-end">
             <FontAwesomeIcon icon={faLock}  />
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -30,30 +30,30 @@ export function NavLogo(){
   return(
     <Navbar bg="light" expand="lg" style={{ height: '120px' }}>
         <Container>
+        
           <Col md={4}>
             <Image src="/logo-pmac.svg" alt="Vercel Logo" className="mt-2" width={222} height={130} />
           </Col>
+          
           <Col md={{ span: 4, offset: 4 }} className="mr-2 text-end">
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="mr-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-            </Nav>
-            <Form className="d-flex ">
-              <FormControl
-                type="search"
-                placeholder="Busca"
-                className="mr-2 text-start"
-                aria-label="Search"
-              />
-            </Form>
-          </Navbar.Collapse>
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="mr-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
+              </Nav>
+              <Form className="d-flex ">
+                <FormControl
+                  type="search"
+                  placeholder="Busca"
+                  className="mr-2 text-start"
+                  aria-label="Search"
+                />
+              </Form>
+            </Navbar.Collapse>
           </Col>
-          
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          
+              <Navbar.Toggle aria-controls="navbarScroll" />
         </Container>
       </Navbar>
     
@@ -66,8 +66,7 @@ export function NavMenu(){
       
     <Navbar bg="light">
         <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
